041c041875810af251eaae016b973a2af44b3231
Dylan.Hu <527419134@qq.com>
2015-10-13 00:15:23 +0800

    初始化Demo项目

---
+++ HBGetContactDemo/HBGetContactDemo/Tools/HBContact/HBGetContact.h
@@ -0,0 +1,16 @@
+//
+//  HBGetContact.h
+//  GetAddress
+//
+//  Created by 胡海波 on 15/10/12.
+//  Copyright © 2015年 胡海波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AddressBook/AddressBook.h>
+
+@interface HBGetContact : NSObject
+
++ (NSMutableArray *)getAllcontactsOfPhone;
+
+@end
---
+++ HBGetContactDemo/HBGetContactDemoTests/HBGetContactDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  HBGetContactDemoTests.m
+//  HBGetContactDemoTests
+//
+//  Created by 胡海波 on 15/10/13.
+//  Copyright © 2015年 Dylan.Hu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HBGetContactDemoTests : XCTestCase
+
+@end
+
+@implementation HBGetContactDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
